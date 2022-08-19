c7397f5bd2e6c73675149b2637286ba48875cd99
Luong Cong Toan <toan@spaceinvaders.io>
2022-08-19 10:56:42 +0700

    + add

---
+++ shark/src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","UI"],collisionMatrix:[[true],[false,true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/loading.fire",orientation:"landscape",jsList:[]};

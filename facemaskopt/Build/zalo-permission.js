406dc0a665d29a5f167ddd175e8047ea43fd19d0
toan.luongcong <pt.team20111@gmail.com>
2023-12-03 16:19:34 +0700

    Update zalo-permission.js

--- facemaskopt/Build/zalo-permission.js
+++ facemaskopt/Build/zalo-permission.js
@@ -1,17 +1,5 @@
-import api from "zmp-sdk";
 function toan_zaloCameraPermission() {
     
-    api.getPhoneNumber({
-        success: async function(data) {
-          // xử lý khi gọi api thành công
-          let { token, number } = data;
-          // xử lý cho trường hợp sử dụng phiên bản Zalo mới
-            window.alert("thanh cong");
-        },
-        fail: function(error) {
-          // xử lý khi gọi api thất bại
-          window.alert("error ");
-        },
-      });
+   
       
 }

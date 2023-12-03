dd645f6dd5bca85976aaf1bd3dd082d94a00d011
toan.luongcong <pt.team20111@gmail.com>
2023-12-03 14:54:30 +0700

    + add permision

---
+++ facemaskopt/Build/zalo-permission.js
@@ -0,0 +1,7 @@
+// Assuming this is in a module or script file
+import zmpSdk from "zmp-sdk/apis";
+
+// Define a global function
+function checkZaloCameraPermission() {
+    window.alert("CheckZaloCameraPermission!!!!");
+}

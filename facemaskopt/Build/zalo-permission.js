d4bdcbc89caac81c21c4979e4d8ba68277ecdc12
toan.luongcong <pt.team20111@gmail.com>
2023-12-03 15:58:56 +0700

    Update zalo-permission.js

--- facemaskopt/Build/zalo-permission.js
+++ facemaskopt/Build/zalo-permission.js
@@ -1,5 +1,5 @@
 // Assuming this is in a module or script file
-import zmpSdk from "zmp-sdk/apis";
+//import zmpSdk from "zmp-sdk/apis";
 
 // Define a global function
 function toan_zaloCameraPermission() {

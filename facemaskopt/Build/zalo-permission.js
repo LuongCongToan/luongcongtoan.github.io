36d2d628c149c78777e7ca56f5774b5477c458ef
toan.luongcong <pt.team20111@gmail.com>
2023-12-03 15:50:05 +0700

    - update

--- facemaskopt/Build/zalo-permission.js
+++ facemaskopt/Build/zalo-permission.js
@@ -1,5 +1,23 @@
 // Assuming this is in a module or script file
+import zmpSdk from "zmp-sdk/apis";
+
 // Define a global function
 function toan_zaloCameraPermission() {
-    window.alert("toan_zaloCameraPermission!!!!");
+    console.log("=== toan_zaloCameraPermission=== ");
+    zmpSdk.requestCameraPermission({
+        success: function(response) {
+          var userAllow = response.userAllow;
+          var message = response.message;
+  
+          if (userAllow) {
+            window.alert("requestCameraPermission!!!!");
+            // được phép sử dụng camera
+          }
+        },
+        fail: function(err) {
+          // xử lý khi gọi api thất bại
+          console.log(err);
+          window.alert("requestCameraPermission!!!! error");
+        }
+      });
 }
